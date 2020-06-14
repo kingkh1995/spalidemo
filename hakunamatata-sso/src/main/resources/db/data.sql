96fdb82f2b76d9710187f713b3bcf22130ed28c1
KaiKoo <kingkh1995@outlook.com>
2020-06-14 19:40:59 +0800

    update data.sql

--- hakunamatata-sso/src/main/resources/db/data.sql
+++ hakunamatata-sso/src/main/resources/db/data.sql
@@ -2,10 +2,10 @@ DELETE FROM user;
 
 INSERT INTO user (id, name, gender, age, email, app_no) VALUES
 (1, '凯', 'MALE', 18, 'kai@hakunamatata.com', 'app1'),
-(2, 'Yu', 'MALE', 20, 'yu@hakunamatata.com', 'app2'),
-(3, 'Ren', 'MALE', 25, 'ren@hakunamatata.com', 'app1'),
-(5, 'Jing', 'FEMALE', 26, 'jing@hakunamatata.com', 'app1'),
-(6, 'Long', 'MALE', 32, 'long@hakunamatata.com', 'app3');
+(2, '宇', 'MALE', 20, 'yu@hakunamatata.com', 'app2'),
+(3, '任', 'MALE', 25, 'ren@hakunamatata.com', 'app1'),
+(5, '静', 'FEMALE', 26, 'jing@hakunamatata.com', 'app1'),
+(6, '龙', 'MALE', 32, 'long@hakunamatata.com', 'app3');
 
 INSERT INTO user (id, name, gender, age, email, app_no, deleted) VALUES
-(4, 'Xu', 'MALE', 26, 'xu@hakunamatata.com', 'app4', 1);
+(4, '旭', 'MALE', 26, 'xu@hakunamatata.com', 'app4', 1);
